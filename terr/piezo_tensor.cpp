a51ae667fbcd9b140582341e4369845ba062d6d8
terr <yury.terekhov@gmail.com>
2012-04-27 14:12:57 +0400

    terr folder created

---
+++ terr/piezo_tensor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "piezo_tensor.h"
+
+using namespace std;
+
+PiezoTensor::PiezoTensor(){
+
+}
+
+std::ostream& operator<<(std::ostream& os, const PiezoTensor& ptens){
+  for(int p = 0; p < 3; ++p){
+    for(int q = 0; q < 3; ++q){
+      for(int r = 0; r < 3; ++r)
+	os << ptens(p, q, r) << " ";
+      os << endl;
+    }
+    cout << endl;
+  }
+  return os;
+}
---
+++ terr/util.h
@@ -0,0 +1,8 @@
+#ifndef UTIL
+#define UTIL
+
+class PiezoTensor;
+
+PiezoTensor makePiezoTensor(double e15, double e22, double e31, double e33);
+
+#endif
