e0a2e9c98970afecc5fd0447f54b056f15caf8e3
Terrum <yury.terekhov@gmail.com>
2012-04-29 12:43:16 +0400

    Terr util files upd

--- terr/util.cpp
+++ terr/util.cpp
@@ -2,14 +2,48 @@
 #include "piezo_tensor.h"
 #include "material_tensor.h"
 
+int magic[3][3] = {{0, 5, 4},
+		   {5, 1, 3},
+		   {4, 3, 2}};
+
 PiezoTensor 
 makePiezoTensor(double e15, double e22, double e31, double e33){
   PiezoTensor ret;
+  double ptc[3][6] = {{0, 0, 0, 0, e15, -e22}, 
+		      {-e22, e22, 0, e15, 0, 0}, 
+		      {e31, e31, e33, 0, 0, 0}};
+  
+  for(int p=0; p<3; ++p)
+    for(int q=0; q<3; ++q)
+      for(int r=0; r<3; ++r)
+	ret(p, q, r) = ptc[p][magic[q][r]];
+  
   return ret;
 }
 
 MaterialTensor 
 makeTrigonalMaterialTensor(double c11, double c12, double c13, double c14, double c33, double c44, double c66){
   MaterialTensor ret;
+  double mtc[6][6] = {{c11, c12, c13, c14, 0, 0}, 
+		      {c12, c11, c13, -c14, 0, 0}, 
+		      {c13, c13, c33, 0, 0, 0},
+		      {c14, -c14, 0, c44, 0, 0}, 
+		      {0, 0, 0, 0, c44, c14}, 
+		      {0, 0, 0, 0, c14, c66}};
+  
+  for(int p=0; p<3; ++p)
+    for(int q=0; q<3; ++q)
+      for(int r=0; r<3; ++r)
+	for(int s=0; s<3; ++s)
+	  ret(p, q, r, s) = mtc[magic[p][q]][magic[r][s]];
+  
   return ret;
+}
+
+double makeEpsilonTensor(double eps11, double eps33){
+  double epstc[3][3] = {{eps11, 0, 0}, 
+			{0, eps11, 0}, 
+			{0, 0, eps33}};
+
+  return epstc[3][3];
 }
--- terr/util.h
+++ terr/util.h
@@ -4,6 +4,13 @@
 class PiezoTensor;
 class MaterialTensor;
 
-PiezoTensor makePiezoTensor(double e15, double e22, double e31, double e33);
-MaterialTensor makeTrigonalMaterialTensor(double c11, double c12, double c13, double c14, double c33, double c44, double c66);
+PiezoTensor 
+makePiezoTensor(double e15, double e22, double e31, double e33);
+
+MaterialTensor 
+makeTrigonalMaterialTensor(double c11, double c12, double c13, double c14, double c33, double c44, double c66);
+
+double 
+makeEpsilonTensor(double eps11, double eps33);
+
 #endif
