f90272efcabca432a196b2faa603a8d178a21c1b
Arseniy Trushin <142857@mail.ru>
2012-11-16 11:55:54 +0400

    polina debug

--- polina/main.cpp
+++ polina/main.cpp
@@ -39,7 +39,7 @@ void coeff_tune() {
   Matrix3 christ = make_christ(n,pt,mt,eps);
 
   //cout << "pt = " << endl << pt << endl;
-  //cout << "mt = " << endl << mt << endl;
+  cout << "mt = " << endl << mt << endl;
   //cout << "eps = " << endl << eps << endl;
 
   Poly3 charact = christ.getCharPoly();
