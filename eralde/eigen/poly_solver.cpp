c5c625e37432ded45997ba9f183010e49934f620
eralde <eralde.g@gmail.com>
2012-12-21 14:47:14 +0400

    junk removed from poly_solver.cpp

--- eralde/eigen/poly_solver.cpp
+++ eralde/eigen/poly_solver.cpp
@@ -10,6 +10,7 @@ using ::std::vector;
 
 using ::Eigen::MatrixXd;
 
+//  Calculates polynomial roots by finding eigenvalue of it's companion matrix.
 void calculateRoots(vector<double> coeffs){
   
   if (0 == coeffs.size()) {
@@ -48,80 +49,12 @@ void calculateRoots(vector<double> coeffs){
   cout << endl << "Eigenvalues:" << solver.eigenvalues().transpose() << endl;
 }
 
-// using namespace ::Eigen;
-
-// typedef ::std::vector<double> VectorXd;
-
-//  Pretty printing
-// ostream&  operator<<(ostream& os, const VectorXd& v) {
-  // cout << "zzz" << v.length();
-  
-  // return os;
-// }
-
 int main() {
-
-  // auto a = vector<double>{1, 2, 3};
-  
-  // cout << a.size() << endl;
-  
   calculateRoots(vector<double>{1, -7, 17, -17, 6});
-  // int m_size = 3;
   
-  // MatrixXd test1(m_size, m_size);
+  calculateRoots(vector<double>{1, 2, 1});
   
-  // for (int i = 0; i < m_size; ++i) {
-    // for (int j = 0; j < m_size; ++j) {
-      // if (i != j) {
-        // test1(i, j) = 0;
-      // }
-      // else {
-        // test1(i, j) = 1;
-      // }
-    // }
-  // }
+  calculateRoots(vector<double>{1, 0, 0, 1});
   
-  // test1(0, 2) = 1;
-  // test1(1, 2) = 2;
-  
-  // EigenSolver<MatrixXd> solver;
-  // solver.compute(test1, /* computeEigenvectors = */ false);
-  
-  // cout << test1 << endl;
-  // cout << "The eigenvalues of A are: " << solver.eigenvalues().transpose() << endl;
-  
-  // calculateRoots(vector<double>{1, 2, 1});
-  
-  
-  // EigenSolver<MatrixXf> es;
-// MatrixXf A = MatrixXf::Random(4,4);
-// es.compute(A, /* computeEigenvectors = */ false);
-// cout << "The eigenvalues of A are: " << es.eigenvalues().transpose() << endl;
-// es.compute(A + MatrixXf::Identity(4,4), false); // re-use es to compute eigenvalues of A+I
-// cout << "The eigenvalues of A+I are: " << es.eigenvalues().transpose() << endl;
-  
-  // test1(0, 0) = 1;
-  // test1(1, 1) = 2;
-  
-  // test1(1, 0) = 0;
-  // test1(0, 1) = 0;
-  
-  // SelfAdjointEigenSolver<MatrixXd> eigensolver(test1);
-  
-  // if (eigensolver.info() != Success) abort();
-  // zzz << 1, 2, 3, 4, 5, 6;
-  
-  // cout << test1;
-  // cout << eigensolver.eigenvalues() << endl;
-// Matrix2f A;
-// A << 1, 2, 2, 3;
-// cout << "Here is the matrix A:\n" << A << endl;
-// SelfAdjointEigenSolver<Matrix2f> eigensolver(A);
-// if (eigensolver.info() != Success) abort();
-// cout << "The eigenvalues of A are:\n" << eigensolver.eigenvalues() << endl;
-// cout << "Here's a matrix whose columns are eigenvectors of A \n"
-// << "corresponding to these eigenvalues:\n"
-// << eigensolver.eigenvectors() << endl;
-
   return 0;
 }
